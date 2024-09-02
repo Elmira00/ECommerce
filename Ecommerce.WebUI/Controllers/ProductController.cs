cca28a44bc70b4f94aaa9e9f03db09769de55382
Elmira <ahmedva.099@gmail.com>
2024-09-03 03:40:03 +0400

    .

--- Ecommerce.WebUI/Controllers/ProductController.cs
+++ Ecommerce.WebUI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Ecommerce.Business.Abstract;
+using Ecommerce.Entities.Models;
 using Ecommerce.WebUI.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -29,8 +30,79 @@ namespace Ecommerce.WebUI.Controllers
             };
             return View(model);
         }
+        static void BubbleSort(List<Product> arr, int sort)
+        {
+            int n = arr.Count;
+            bool swapped;
+            if (sort == 0)
+            {
 
+                for (int i = 0; i < n - 1; i++)
+                {
+                    swapped = false;
 
+                    for (int j = 0; j < n - i - 1; j++)
+                    {
+                        // Compare first letters of the words
+                        if (arr[j].ProductName[0] > arr[j + 1].ProductName[0])
+                        {
+                            // Swap arr[j] and arr[j + 1]
+                            Product temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+
+                            swapped = true;
+                        }
+                    }
+
+                    // If no elements were swapped in the inner loop, the array is already sorted
+                    if (!swapped)
+                        break;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    swapped = false;
+
+                    for (int j = 0; j < n - i - 1; j++)
+                    {
+                        // Compare first letters of the words
+                        if (arr[j].ProductName[0] < arr[j + 1].ProductName[0])
+                        {
+                            // Swap arr[j] and arr[j + 1]
+                            Product temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+
+                            swapped = true;
+                        }
+                    }
+
+                    // If no elements were swapped in the inner loop, the array is already sorted
+                    if (!swapped)
+                        break;
+                }
+            }
+        }
+        public async Task<ActionResult> Sort(int page, int category, int sorting, int count, int size)
+        {
+            int pageSize = 10;
+            var items = await _productService.GetAllByCategoryAsync(category);
+            var myProducts = items.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            BubbleSort(myProducts, sorting);
+            var model = new SortProductsListViewModel
+            {
+                Products = myProducts,
+                Page = page,
+                Category = category,
+                Sorting = sorting,
+                Size = size,
+                Count = count
+            };
+            return View(model);
+        }
         // GET: ProductController/Details/5
         public ActionResult Details(int id)
         {
---
+++ Ecommerce.WebUI/Models/SortProductsListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Entities.Models;
+
+namespace Ecommerce.WebUI.Models
+{
+    public class SortProductsListViewModel
+    {
+        public List<Product> Products { get; set; }
+        public int Page { get; set; }
+        public int Category { get; set; }
+        public int Sorting { get; set; }
+        public int Count { get; set; }
+        public int Size { get; set; }
+    }
+}
